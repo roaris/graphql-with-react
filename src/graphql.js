21b14726efd148d0b5c2c22fce6dc293794f2c01
roaris <one0803kyopro@gmail.com>
2022-02-17 22:13:57 +0900

    searchクエリを追加

--- src/graphql.js
+++ src/graphql.js
@@ -1,9 +1,29 @@
 import { gql } from 'apollo-boost';
 
-export const ME = gql`
-  query me {
-    user(login: "roaris") {
-      name
+export const SEARCH_REPOSITORIES = gql`
+  query searchRepositories($first: Int, $after: String, $last: Int, $before: String, $query: String!) {
+    search(first: $first, after: $after, last: $last, before: $before, query: $query, type: REPOSITORY) {
+      repositoryCount
+      pageInfo {
+        endCursor
+        hasNextPage
+        hasPreviousPage
+        startCursor
+      }
+      edges {
+        cursor
+        node {
+          ... on Repository {
+            id
+            name
+            url
+            stargazers {
+              totalCount
+            }
+            viewerHasStarred
+          }
+        }
+      }
     }
   }
 `
