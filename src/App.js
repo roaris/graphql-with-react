0b024075ed3353570a80126eba5b5b4cf443d040
roaris <one0803kyopro@gmail.com>
2022-02-17 22:32:21 +0900

    検索フォームを追加

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { Query } from 'react-apollo';
 import client from './client';
 import { SEARCH_REPOSITORIES } from './graphql'
 
-const VARIABLES = {
+const DEFAULT_STATE = {
   first: 5,
   after: null,
   last: null,
@@ -13,10 +13,22 @@ const VARIABLES = {
 };
 
 const App = () => {
-  const [variables, setVariables] = useState(VARIABLES);
+  const [variables, setVariables] = useState(DEFAULT_STATE);
   const { query, first, last, before, after } = variables;
+  console.log({query});
+
+  const handleChange = (e) => {
+    setVariables({
+      ...variables,
+      query: e.target.value
+    }) // スプレッド構文
+  };
+
   return (
     <ApolloProvider client={client}>
+      <form>
+        <input value={query} onChange={handleChange}></input>
+      </form>
       <Query
         query={SEARCH_REPOSITORIES}
         variables={{ query, first, last, before, after}}
