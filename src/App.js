6f03d81efa3e7cc805a9fb0d328afd9fb4bada11
roaris <one0803kyopro@gmail.com>
2022-02-17 22:48:17 +0900

    レポジトリの件数を表示

--- src/App.js
+++ src/App.js
@@ -37,8 +37,12 @@ const App = () => {
           ({ loading, error, data }) => {
             if (loading) return 'Loading...';
             if (error) return `Error! ${error.message}`
-            console.log({data});
-            return <div></div>
+
+            const search = data.search;
+            const repositoryCount = search.repositoryCount;
+            const repositoryUnit = repositoryCount === 1 ? 'Repository' : 'Repositories';
+            const title = `GitHub Repositories Search Results - ${repositoryCount} ${repositoryUnit}`
+            return <h1>{title}</h1>
           }
         }
       </Query>
