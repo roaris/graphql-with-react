004516d6737d1fae9605d8f280b515e03e2c0f82
roaris <one0803kyopro@gmail.com>
2022-02-17 21:39:25 +0900

    ApolloClientのセットアップ

--- src/App.js
+++ src/App.js
@@ -1,10 +1,33 @@
-const GITHUB_TOKEN = process.env.REACT_APP_GITHUB_TOKEN;
+import React from 'react';
+import { ApolloProvider } from 'react-apollo';
+import { gql } from 'apollo-boost';
+import { Query } from 'react-apollo';
+import client from './client';
 
-console.log({GITHUB_TOKEN});
+const ME = gql`
+  query me {
+    user(login: "roaris") {
+      name
+
+    }
+  }
+`
 
 function App() {
   return (
-    <div>Hello, GraphQL</div>
+    <ApolloProvider client={client}>
+      <div>Hello, GraphQL</div>
+      <Query query={ME}>
+        {
+          ({ loading, error, data }) => {
+            if (loading) return 'Loading...';
+            if (error) return `Error! ${error.message}`
+
+            return <div>{data.user.name}</div>
+          }
+        }
+      </Query>
+    </ApolloProvider>
   );
 }
 
